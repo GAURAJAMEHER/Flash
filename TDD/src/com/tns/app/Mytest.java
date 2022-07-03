49f96b8fb234ddb3346df087ab9981e9bb086341
HP <HP@LAPTOP-6NF6KKTP>
2022-07-03 22:50:04 +0530

    fIRST

---
+++ TDD/src/com/tns/app/Mytest.java
@@ -0,0 +1,37 @@
+package com.tns.app;
+
+import com.tns.app.*;
+
+class Normalacc{
+public class Gsnormal extends Normalacc {
+	//constructor
+		public void Gsnormalacc(int accNo, String accNm, float charges, float deliveryCharges) {
+			
+		}
+		
+		//methods
+		@Override
+		public void bookProduct(float charges) {
+//			
+			System.out.println("For Normal user Charges are: " + getDeliveryCharges() + " with Delivery charge: "+ getDeliveryCharges());
+		}
+
+		
+		@Override
+		public String toString() {
+			return super.toString();
+		}
+
+	}
+
+public void bookProduct(float charges) {
+	// TODO Auto-generated method stub
+	
+}
+
+public String getDeliveryCharges() {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
+
